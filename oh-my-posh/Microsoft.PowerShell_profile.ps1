2cf396d48aea6a9aa99a39ce77feeab086df4cae
Garrett Phelps <garrphelps@gmail.com>
2026-03-16 19:09:49 -0500

    More ls commands

--- oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,11 @@
-oh-my-posh init pwsh | Invoke-Expression
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
 Remove-Item Alias:ls -Force -ErrorAction SilentlyContinue
 Set-Alias ls lsd
+# Enable lsd aliases (optional)
+function la { lsd -a $args }
+function ll { lsd -l $args }
+function lla { lsd -la $args }
+function lt { lsd --tree $args }
+
+oh-my-posh init pwsh | Invoke-Expression
